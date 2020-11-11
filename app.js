a5739179b44f5c95af998239fdefea2c69dc3dd7
mariegross <marie.gross@gmx.de>
2020-11-11 09:53:34 +0100

    Turn then function into async function, add validateAccess and function to overwrite db.json

--- app.js
+++ app.js
@@ -6,22 +6,61 @@
 // args["wifi"];
 
 const inquirer = require("inquirer");
+const fs = require("fs").promises;
+
 const mpw = "0000";
+
 const questions = [
   {
     type: "password",
     name: "masterPassword",
     message: "What's your password?",
   },
+  //   {
+  //     type: "password",
+  //     name: "masterPassword",
+  //     message: "What's your password?",
+  //   },
 ];
 
-inquirer.prompt(questions).then((answers) => {
-  if (answers.masterPassword === mpw) {
-    console.log("Password is correct.");
-  } else {
-    console.log("Password is incorrect.");
+async function validateAccess() {
+  const { masterPassword } = await inquirer.prompt(questions);
+  const passwordSafeJSON = await fs.readFile("./db.json", "utf8");
+  const passwordSafe = JSON.parse(passwordSafeJSON);
+
+  // inquirer.prompt(questions).then((answers) => {
+  if (masterPassword !== mpw) {
+    console.error("Password is incorrect.");
+    validateAccess();
+    return;
   }
-});
+
+  const args = process.argv.slice(2);
+  const passwordName = args[0];
+  const newPasswordValue = args[1];
+
+  if (newPasswordValue) {
+    passwordSafe[passwordName] = newPasswordValue;
+    fs.writeFile("./db.json", JSON.stringify(passwordSafe, null, 2));
+  } else {
+    console.log(`You want to know the password of '${passwordName}'.`);
+    const password = passwordSafe[passwordName];
+    if (password) {
+      console.log(`Password is ${password}`);
+    } else {
+      console.log("Unknown password.");
+    }
+  }
+}
+validateAccess();
+
+//   try {
+//     const data = JSON.parse(fs.readFileSync("./db.json", "utf8"));
+//     console.log(data);
+//   } catch (err) {
+//     console.error(err);
+//   }
+// });
 
 // const args = process.argv.slice(2);
 // const passwordName = args[0];
