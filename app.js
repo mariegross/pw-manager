b1dcde11ae2c63eeaff3d793182dfdbf07b013d8
mariegross <marie.gross@gmx.de>
2020-11-13 12:06:20 +0100

    Connect MongoDB and import dotenv

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const { readCommandLineArguments } = require("./lib/commandLine");
+const { connect, close } = require("./lib/database");
 const { getPassword, setPassword } = require("./lib/passwords");
 const { askForMasterPassword } = require("./lib/questions");
 const { isMasterPasswordCorrect } = require("./lib/validation");
+require("dotenv").config();
+// const MongoClient = require("mongodb").MongoClient;
+// const assert = require("assert");
 
 async function run() {
+  console.log("Connecting to database...");
+  await connect(
+    `mongodb+srv://${process.env.MONGODB_PASSWORD}@cluster0.5v6ea.mongodb.net/pw-manager?retryWrites=true&w=majority`,
+    "pw-manager"
+  );
+  console.log("Connected to database.");
+
   const masterPassword = await askForMasterPassword();
   if (!(await isMasterPasswordCorrect(masterPassword))) {
     console.error("Password is incorrect.");
@@ -23,5 +34,6 @@ async function run() {
     const passwordValue = await getPassword(passwordName);
     console.log(`Your password is ${passwordValue}.`);
   }
+  await close();
 }
 run();
