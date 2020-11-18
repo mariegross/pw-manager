6ec6e3f7a63c77ac79a06e337db44f36698edb66
mariegross <marie.gross@gmx.de>
2020-11-18 13:46:08 +0100

    Update App.js in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { getPassword } from "./api/passwords";
 
 function App() {
+  const [password, setPassword] = useState(null);
+
+  useEffect(() => {
+    const doFetch = async () => {
+      const newPassword = await getPassword("wifi");
+      setPassword(newPassword);
+    };
+    doFetch();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {password}
       </header>
     </div>
   );
