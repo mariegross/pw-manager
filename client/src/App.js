014d0bdc84a390a51bbb933b0c8a420f65acbc15
mariegross <marie.gross@gmx.de>
2020-11-18 13:57:32 +0100

    Add error handling

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,21 @@ import { getPassword } from "./api/passwords";
 function App() {
   const [password, setPassword] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const doFetch = async () => {
-      setLoading(true);
-      const newPassword = await getPassword("wifi");
-      setPassword(newPassword);
-      setLoading(false);
+      try {
+        setLoading(true);
+        setError(null);
+        const newPassword = await getPassword("wifi");
+        setPassword(newPassword);
+      } catch (error) {
+        console.error(error);
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
     };
     doFetch();
   }, []);
@@ -22,6 +30,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         {loading && <div>Loading...</div>}
+        {error && <div>{error.message}</div>}
         {password}
       </header>
     </div>
