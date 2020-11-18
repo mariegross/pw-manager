dd3e444fb655d2cb472b20ac5afe1807f3e58137
mariegross <marie.gross@gmx.de>
2020-11-18 13:50:22 +0100

    Add loading state

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,14 @@ import { getPassword } from "./api/passwords";
 
 function App() {
   const [password, setPassword] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const doFetch = async () => {
+      setLoading(true);
       const newPassword = await getPassword("wifi");
       setPassword(newPassword);
+      setLoading(false);
     };
     doFetch();
   }, []);
@@ -18,6 +21,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {loading && <div>Loading...</div>}
         {password}
       </header>
     </div>
